e5b5db9f3e1eaa14b609fe68bf0de6e19b6689d0
Dmitrii Volkov <d.volkov@geophy.com>
2025-06-03 15:46:14 +0200

    SUPER test

--- tests/integration/data_sources_and_expectations/data_sources/test_redshift.py
+++ tests/integration/data_sources_and_expectations/data_sources/test_redshift.py
@@ -41,3 +41,27 @@ class TestRedshiftDataTypes:
                 )
             )
         assert result.success
+
+    @pytest.mark.redshift
+    def test_super(self):
+        column_type = REDSHIFT_TYPES.SUPER
+        batch_setup = RedshiftBatchTestSetup(
+            config=RedshiftDatasourceTestConfig(column_types={self.COLUMN: column_type}),
+            data=pd.DataFrame(
+                {
+                    self.COLUMN: [
+                        "{ \"type\": \"Point\", \"coordinates\": [1.0, 2.0] }"
+                    ]
+                }
+            ),
+            extra_data={},
+            context=get_context(mode="ephemeral"),
+        )
+        with batch_setup.batch_test_context() as batch:
+            result = batch.validate(
+                expect=ExpectColumnValuesToBeOfType(
+                    column=self.COLUMN,
+                    type_="SUPER",
+                )
+            )
+        assert result.success
